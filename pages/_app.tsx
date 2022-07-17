bf9103a6a98329942b9573a388c3318dce84accd
aksarlaode <akhsaras21@gmail.com>
2022-07-17 12:37:03 +0000

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import "../styles/globals.css";
+
+import { globalStyles, ThemeProvider } from '@laodeaksarr/design-system';
+
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+
+//import { Head } from '~/components/Seo';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  globalStyles();
+
+  return (
+    <ThemeProvider>
+      {/*<Head />*/}
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ThemeProvider>
+  );
+};
+export default App;
